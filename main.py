ff95da82510f8ccbf0d56dde4804fc2c9bbd845d
Arthurgaschet <131858236+Arthurgaschet@users.noreply.github.com>
2024-11-19 17:18:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
+
 df = pd.read_excel('top_100_dishes.xlsx')
 
 # Titre principal de l'application
